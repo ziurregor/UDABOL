36d47d49ae789424087f0faf155aedc151755740
Carlos Morales <71267202+Carlos00001@users.noreply.github.com>
2020-09-18 22:50:06 -0400

    Add files via upload

---
+++ mravi/meneceswilfredo/tarea.java
@@ -0,0 +1,37 @@
+ 
+import java.util.*;
+
+public class tarea {
+    public static void main(String[] args) {
+        Scanner muestra = new Scanner(System.in);
+        int n = muestra.nextInt();
+	int [] parent = new int[n+1];
+	int [] perc = new int [n+1];
+	int [] issuper = new int [n+1];
+        for (int c=1;c<n;c++) {
+            int a = muestra.nextInt();
+            int b = muestra.nextInt();
+            parent[b] = a;
+            perc[b] = muestra.nextInt();
+            issuper[b] = muestra.nextInt();
+        }
+        double m = 0;
+        for (int i =1;i<n+1;i++) {
+            double ans = muestra.nextDouble();
+            if (ans>=0) {
+                int j =i;
+                while (j>1){
+                    if (issuper[j]==1) {
+                        ans = Math.sqrt(ans);
+                    }
+                    ans *= (100.0/perc[j]);
+                    j= parent[j];
+                }
+                if (ans>m) {
+                    m=ans;
+                }
+            }
+        }
+        System.out.println(m);
+    }
+}
