01c7ef379835f0a1574de19ebdea0813de5c574d
Joel Hinojosa Rojas <71051062+JOEL-H-CMD@users.noreply.github.com>
2020-09-29 11:56:10 -0400

    Clapa de Negocio

---
+++ GrupoModelo2/AsignadorTareasAPI/Manejadores/ManejadorTareas.cs
@@ -0,0 +1,75 @@
+﻿using DAO;
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Negocio
+{
+    public class ManejadorTareas : IManejadorTareas
+    {
+        private IBaseDeDatos _context;
+        public ManejadorTareas(IBaseDeDatos context)
+        {
+            _context = context;
+        }
+
+        public Tarea ActualizarTarea(int id, Tarea tarea)
+        {
+            return _context.Tarea().Editar(id, tarea);
+        }
+
+        public Tarea CrearTarea(Tarea tarea)
+        {
+            return _context.Tarea().Crear(tarea);
+        }
+
+        public bool EliminarTarea(int id)
+        {
+            return _context.Tarea().Eliminar(id);
+        }
+
+        public MostrarTarea ObtenerTareaPorID(int id)
+        {
+            return ObtenerTareas().FirstOrDefault(ot => ot.TareaID == id);
+        }
+
+        public IEnumerable<MostrarTarea> ObtenerTareas()
+        { 
+            IEnumerable<Tarea> tareas = _context.Tarea().ObtenerTodo();
+            IEnumerable<EstadoTarea> estadosTareas = _context.EstadoTarea().ObtenerTodo();
+            IEnumerable<Persona> personas = _context.Persona().ObtenerTodo();
+            IEnumerable<Usuario> usuarios = _context.Usuario().ObtenerTodo();
+            IEnumerable<Estado> estados = _context.Estado().ObtenerTodo();
+            return tareas.Join(estadosTareas, tarea => tarea.EstadoTareaID, estadoTarea => estadoTarea.EstadoTareaID,
+                (tarea, estadoTarea) => new { tarea, estadoTarea })
+                .Join(personas, te => te.tarea.PersonaID, persona => persona.PersonaID,
+                (te, persona) => new { te.tarea, te.estadoTarea, persona })
+                .Join(usuarios, tep => tep.persona.UsuarioID, usuario => usuario.UsuarioID,
+                (tep, usuario) => new { tep.tarea, tep.estadoTarea, tep.persona, usuario})
+                .Join(estados, teps => teps.estadoTarea.EstadoID, estado => estado.EstadoID,
+                (teps, estado) => new { teps.tarea, teps.estadoTarea, teps.persona, teps.usuario, estado })
+                .Select(tepse => new MostrarTarea
+                {
+                    TareaID = tepse.tarea.TareaID,
+                    NombreTarea = tepse.tarea.NombreTarea,
+                    Fecha = tepse.tarea.Fecha,
+                    EstadoTarea = new MostrarEstadoTarea 
+                    {
+                        EstadoTareaID = tepse.tarea.EstadoTareaID,
+                        EstadoID = tepse.estadoTarea.EstadoID,
+                        NombreEstado = tepse.estado.NombreEstado
+                    },
+                    Usuario = new MostrarUsuario
+                    {
+                        PersonaID = tepse.persona.PersonaID,
+                        Nombres = tepse.persona.Nombres,
+                        Apellidos = tepse.persona.Apellidos,
+                        UsuarioID = tepse.usuario.UsuarioID,
+                        Username = tepse.usuario.Username
+                    }
+                });
+        }
+    }
+}
---
+++ GrupoModelo2/AsignadorTareasAPI/ModelosDeRespuesta/MostrarTarea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Negocio
+{
+    public class MostrarTarea
+    {
+        public int TareaID { get; set; }
+        public string NombreTarea { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public MostrarUsuario Usuario { get; set; }
+
+        public MostrarEstadoTarea EstadoTarea { get; set; }
+
+    }
+}
