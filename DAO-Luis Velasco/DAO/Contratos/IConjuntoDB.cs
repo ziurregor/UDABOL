400c11059afe83263a095085d05c933b2dd49a49
luisfernandovelasco <72094237+luisfernandovelasco@users.noreply.github.com>
2020-09-29 12:36:21 -0400

    Add files via upload

---
+++ DAO-Luis Velasco/DAO/Contratos/IConjuntoDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAO
+{
+    public interface IConjuntoDB<T>
+    {
+        public IEnumerable<T> ObtenerTodo();
+        public T ObtenerUno(int id);
+        public T Crear(T elemento);
+        public T Editar(int id, T elemento);
+        public bool Eliminar(int id);
+        public bool GuardarCambios();
+    }
+}
